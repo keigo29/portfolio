9356a225d88e75df23e0d8bb76e764c9f60f13bb
kegio29 <21im0015@i-u.ac.jp>
2024-12-27 21:36:49 +0900

    色々更新

--- js/slider.js
+++ js/slider.js
@@ -1,19 +1,19 @@
-// slick01
-
-
-// $('.slick-center').slick({
-//   centerMode: true,
-//   centerPadding: '0px',
-//   slidesToShow: 1,
-
-// });
-
 const swiper = new Swiper(".swiper", {
-  loop: true, // ループ有効
-  slidesPerView: 3, // 一度に表示する枚数
-  speed: 6000, // ループの時間
-  allowTouchMove: false, // スワイプ無効
-  autoplay: {
-    delay: 0, // 途切れなくループ
+  slidesPerView: 1,
+    breakpoints: {
+    // 768px以上の場合
+    768: {
+      slidesPerView: 5,
+    },
   },
+  // ページネーションが必要なら追加
+  pagination: {
+    el: ".swiper-pagination"
+  },
+
+  // ナビボタンが必要なら追加
+  // navigation: {
+  //   nextEl: ".swiper-button-next",
+  //   prevEl: ".swiper-button-prev"
+  // }
 });
