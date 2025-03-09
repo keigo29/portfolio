f1a90c595a636b2df5bd220bffc1ae30e3937fcd
kegio29 <21im0015@i-u.ac.jp>
2025-03-09 17:02:29 +0900

    microcms導入

--- js/main.js
+++ js/main.js
@@ -35,3 +35,54 @@ $("#g-nav a").click(function () {//ナビゲーションのリンクがクリッ
 });
 
 
+document.addEventListener("DOMContentLoaded", async () => {
+    const API_URL = "https://keigo-portfolio.microcms.io/api/v1/works";
+    const API_KEY = "QYZiGK3dbkOvEqmju12WLPBNLuY8cczgBdtB";
+
+    try {
+        const response = await fetch(API_URL, {
+            headers: { "X-MICROCMS-API-KEY": API_KEY }
+        });
+
+        if (!response.ok) throw new Error("Failed to fetch data");
+
+        const data = await response.json();
+        const works = data.contents;
+        const swiperWrapper = document.querySelector(".swiper-wrapper");
+
+        works.forEach(work => {
+            const slide = document.createElement("div");
+            slide.classList.add("swiper-slide");
+            const MAX_LENGTH = 90;
+            const displayText = work.dis 
+                ? (work.dis.length > MAX_LENGTH 
+                    ? work.dis.slice(0, MAX_LENGTH) + '…' 
+                    : work.dis)
+                : "";
+            slide.innerHTML = `
+                <div class="Works__items__item">
+                    <a href="https://portfolio-blog-bay-nu.vercel.app/work/${work.id}">
+                        <div class="Works__items__item__head">
+                            <img src="${work.eyecatch.url}" alt="${work.title}">
+                        </div>
+                        <h3 class="Works__items__item__ttl">${work.title}</h3>
+                        <div class="Works__items__item__categoris">
+                            ${work.option.map(category => `<div class="Works__items__item__categoris__categori">${category}</div>`).join('')}
+                        </div>
+                        <div class="Works__items__item__content">
+                             ${displayText.replace(/\n/g, "<br>")}
+                        </div>
+                    </a>
+                </div>
+            `;
+
+            swiperWrapper.appendChild(slide);
+        });
+
+        // Swiper初期化
+
+    } catch (error) {
+        console.error("Error fetching MicroCMS data:", error);
+    }
+});
+
